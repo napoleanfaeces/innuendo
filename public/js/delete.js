443fb7fbf285cdaabea497a93d7fd4b29529cd32
napoleanfaeces <90397553+napoleanfaeces@users.noreply.github.com>
2022-08-21 14:41:55 +0800

    Update delete.js

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,7 +1,7 @@
 function deleteAccount() {
     var deleteProfile = new XMLHttpRequest();
 
-    deleteProfile.open("DELETE", "http://127.0.0.1:8080/users", true);
+    deleteProfile.open("DELETE", "http://ec2-18-232-209-130.compute-1.amazonaws.com:8080/users", true);
     deleteProfile.setRequestHeader("Content-Type", "application/json");
     var payload = {email:email};
     deleteProfile.send(JSON.stringify(payload));
