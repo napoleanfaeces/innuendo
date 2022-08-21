13100247b838929ce3b4216b5abbdf035eae26a9
napoleanfaeces <90397553+napoleanfaeces@users.noreply.github.com>
2022-08-21 14:42:27 +0800

    Update loadProfile.js

--- public/js/loadProfile.js
+++ public/js/loadProfile.js
@@ -5,7 +5,7 @@ const { deleteUser } = require("../../controllers/userController");
 function showAccountDetails() {
     var getProfile = new XMLHttpRequest();
 
-    getProfile.open("POST", "http://127.0.0.1:8080/getUser", true);
+    getProfile.open("POST", "http://ec2-18-232-209-130.compute-1.amazonaws.com:8080/getUser", true);
     getProfile.setRequestHeader("Content-Type", "application/json");
     getProfile.onload = function() {
         profile = JSON.parse(getProfile.responseText);
